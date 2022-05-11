36dcc75726f9e6dc6404861f77dc8b991477f3c5
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-11 19:45:09 -0300

    Aula sobre operadores em Python

---
+++ CodeOne/Python/operadores.py
@@ -0,0 +1,51 @@
+# > Operações Matemáticas (Aritméticas)
+
+"""
+  - Soma: +
+  - Subtração: -
+  - Multiplicação: *
+  - Divisão: /
+  - Divisão inteira: //
+  - Resto da divisão: %
+  - Potência: **
+"""
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 // 3)
+print(20 % 3)
+print(2 ** 3)
+
+# OPERADORES BOOLEANOS
+
+# Os operadores booleanos são operadores de comparação.
+
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print("Python" == "python")
+print("banana" != "abacaxi")
+print(altura1 >= altura2)
+print(altura2 > altura3)
+
+# Pedir para imprimir uma comparação imprime o resultado desta comparação
+"""
+  os resultados das comparações acima são:
+  False
+  True
+  False
+  True
+  True
+  False
+"""
