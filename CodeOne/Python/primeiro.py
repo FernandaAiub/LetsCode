e4d0b91954d9a92c3defa060a08297c40d0150ee
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-11 19:01:23 -0300

    Primeiro programa em Python

---
+++ CodeOne/Python/primeiro.py
@@ -0,0 +1,8 @@
+# > PRIMEIRO CÓDIGO
+
+# print: uma função para imprimir alguma coisa na tela
+print("Hello World!")
+print("Bem vinda(o) ao curso de Python do Coding Tank!")
+
+# input: uma função que pede dados ao usuário
+input("Qual é a linguagem de programação que você está estudando?")
