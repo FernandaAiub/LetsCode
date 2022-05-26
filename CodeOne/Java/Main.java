5c82f353b2409c85ac3a7341ea991e65af92a1b6
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-26 02:08:02 -0300

    Adição de exemplos de uso de operadores booleanos

--- CodeOne/Java/Main.java
+++ CodeOne/Java/Main.java
@@ -2,11 +2,15 @@ public class Main {
 
   public static void main(String[] args) {
 
+    // Exemplo de uso de variáveis do tipo string
+
     String nome;
     nome = "Let's Code";
     nome = "Fernanda";
     nome = "Brasil";
     System.out.println("Olá, " + nome);
+
+    // Exemplo de uso de variáveis numéricas
 
     int a;
     int b = 5;
@@ -20,5 +24,39 @@ public class Main {
     System.out.println(subtracao);
     System.out.println(multiplicacao);
     System.out.println(divisao);
+
+    // Exemplo de uso de operadores booleanos
+
+    boolean resultado = true;
+    System.out.println(resultado);
+
+    // Algoritmo ida à praia
+
+    boolean fimDeSemana = true;
+    boolean fazendoSol = true;
+    boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+    System.out.println(vamosAPraia);
+
+    // Tabela Verdade
+
+    // Operador && (AND)
+    // true && true = true
+    // true && false = false
+    // false && true = false
+    // false && false = false
+
+    // Operador || (OR)
+    // true || true = true
+    // true || false = true
+    // false || true = true
+    // false || false = false
+
+    // Uso do operador ternário (?)
+    // O operador ternário avalia se um item lógico é verdadeiro ou não.
+    // Dependendo do resultado o operador ternário traz um retorno diferente.
+
+    String mensagem = fimDeSemana ? "É fim de semana! :D" : "Não é fim de semana! :(";
+    System.out.println(mensagem);
   }
 }
