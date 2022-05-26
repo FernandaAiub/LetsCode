cb33a276ede30f4da8e63a401ee95a4f294f4f37
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-26 01:43:31 -0300

    Exemplos de uso de variáveis em Java

---
+++ CodeOne/Java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    String nome;
+    nome = "Let's Code";
+    nome = "Fernanda";
+    nome = "Brasil";
+    System.out.println("Olá, " + nome);
+
+    int a;
+    int b = 5;
+    a = 4;
+    int soma = a + b;
+    int subtracao = a - b;
+    int multiplicacao = a * b;
+    float divisao = (float) a / b;
+
+    System.out.println(soma);
+    System.out.println(subtracao);
+    System.out.println(multiplicacao);
+    System.out.println(divisao);
+  }
+}
